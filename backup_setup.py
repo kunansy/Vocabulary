9278a6918154785a9572d02281671426d3865ede
Kirill <anonymouse_name@mail.ru>
2020-02-29 19:36:43 +0300

    Добавлена ловля ошибок;

--- backup_setup.py
+++ backup_setup.py
@@ -1,59 +1,57 @@
 from backup import Auth
 from backup import print_items
+from apiclient import errors
+
+drive_client = Auth()
 
 
 def backup(f_name, f_path):
     """ Залить файл на диск """
-    content_backup = Auth()
-    last_backup = content_backup.search_item(f_name)
+    last_backup = drive_client.search_item(f_name)
 
     if len(last_backup):
-        del_file(last_backup[0]['id'])
-    id = content_backup.upload_file(f_name, f_path)
-    print(f"File: '{f_path}' successfully uploaded, ID: '{id}'")
+        del_item(last_backup[0]['id'])
+    try:
+        id = drive_client.upload_file(f_name, f_path)
+    except Exception as trouble:
+        print(trouble, f_path, 'Terminating', sep='\n')
+    else:
+        print(f"File: '{f_path}' successfully uploaded, ID: '{id}'")
 
 
 def restore(f_name, f_path):
     """ Восстановить файл по имени """
-    restore = Auth()
+    item = drive_client.search_item(f_name)
+
+    if not item:
+        print(f"File to restore: '{f_name}' not found\nTerminating")
+        return
+    if len(item) > 1:
+        print(f"There are more than one file named: '{f_name}', "
+              f"restore cannot be done\nTerminating")
+        return
+
+    id = item[0]['id']
     try:
-        id = restore.search_item(f_name)[0]['id']
-    except:
-        print(f"Item to restore: '{f_name}' does not exist\nTerminating ")
+        drive_client.download_file(id, f_path)
+    except Exception as trouble:
+        print(trouble, (f_name, id), 'Terminating', sep='\n')
     else:
-        restore.download_file(id, f_path)
         print(f"File: '{f_path}' successfully restored")
 
 
-def list_files(count=10):
+def list_items(count=10):
     """ Показать первые n айтемов """
-    content = Auth()
-    print_items(content.list_items(count))
+    print_items(drive_client.list_items(count))
 
 
-def search_items(name):
-    """ Найти айтем по имени """
-    content = Auth()
-    print_items(content.search_item(name))
-
-
-def del_file(f_id):
+def del_item(f_id):
     """ Удалить айтем по ID """
-    delete = Auth()
     try:
-        delete.del_file(f_id)
-    except:
-        print(f"Something went wrong deleting item: '{f_id}'\nTerminating")
+        drive_client.del_item(f_id)
+    except errors.HttpError as trouble:
+        print(trouble, f_id, 'Terminating', sep='\n')
+    except Exception as trouble:
+        print(trouble, f_id, 'Terminating', sep='\n')
     else:
         print(f"Item: '{f_id}' deleted permanently")
-
-
-def download_file(f_id, f_path):
-    """ Скачать файл по ID """
-    download = Auth()
-    try:
-        download.download_file(f_id, f_path)
-    except:
-        print(f"Something went wrong downloading item: '{f_id}'\nTerminating")
-    else:
-        print(f"File: '{f_path}' successfully downloaded")
