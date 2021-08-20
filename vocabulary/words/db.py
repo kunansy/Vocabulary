47bc64569063e8aee7b612a6ed5f4b8748ca786a
kunansy <kolobov.kirill@list.ru>
2021-08-20 19:47:17 +0300

    db updated

--- vocabulary/words/db.py
+++ vocabulary/words/db.py
@@ -44,7 +44,7 @@ async def get_words_to_learn(*,
                              limit: Optional[int] = None,
                              offset: Optional[int] = None) -> list[RowMapping]:
     stmt = sa.select(models.WordToLearn)\
-        .order_by(models.WordToLearn.added_at)\
+        .order_by(models.WordToLearn.c.added_at)\
         .limit(limit).offset(offset)
 
     async with database.session() as ses:
@@ -55,7 +55,7 @@ async def delete_word_to_learn(*,
                                word_id: UUID) -> Optional[RowMapping]:
     stmt = sa.delete(models.WordToLearn)\
         .returning(models.WordToLearn)\
-        .where(models.WordToLearn.word_id == word_id)
+        .where(models.WordToLearn.c.word_id == str(word_id))
 
     async with database.session() as ses:
         return (await ses.execute(stmt)).mappings().one_or_none()
@@ -63,7 +63,7 @@ async def delete_word_to_learn(*,
 
 async def add_word_to_learn(*,
                             word: str) -> None:
-    stmt = sa.insert(models.WordToLearn)\
+    stmt = models.WordToLearn.insert()\
         .values(word=word)
 
     async with database.session() as ses:
