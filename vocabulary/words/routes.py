f63ce4dcf1aa694c41c9bbecb5460d9a40d825bc
kunansy <kolobov.kirill@list.ru>
2021-08-20 15:51:46 +0300

    /words routes started

--- vocabulary/words/routes.py
+++ vocabulary/words/routes.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from uuid import UUID
+
+from fastapi import APIRouter, Query
+
+from vocabulary.words import schemas, db
 
 
 router = APIRouter(
     prefix="/words",
     tags=['words']
 )
+
+
+@router.get('/to-learn',
+            response_model=schemas.WordsToLearnListing)
+async def get_words_to_learn(p: int = Query(1, ge=1),
+                             page_size: int = Query(10, ge=1)):
+    """ List words to learn """
+    pass
+
+
+@router.delete('/to-learn/{word_id}',
+               response_model=schemas.WordToLearn)
+async def remove_word_to_learn(word_id: UUID):
+    """ Remove the word """
+    pass
