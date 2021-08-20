c7fbfe0e8104ced68337ba2318a4d99a21845424
kunansy <kolobov.kirill@list.ru>
2021-08-20 19:59:57 +0300

    /examples routes started

--- vocabulary/examples/routes.py
+++ vocabulary/examples/routes.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+
+from vocabulary.examples import schemas
+from fastapi import APIRouter, Query
+
+from vocabulary.examples import schemas
 
 
 router = APIRouter(
     prefix="/examples",
     tags=['examples']
 )
+
+
+@router.get('/coprus/{word}',
+            response_model=schemas.CorpusExamples)
+async def get_corpus_examples(word: str,
+                              language: schemas.LANGUAGES = Query('en')):
+    pass
+
+
+@router.get('/self/{word}',
+            response_model=schemas.SelfExamples)
+async def get_self_examples(word: str):
+    pass
