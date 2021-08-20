5265e8f84bceca4dfc20c996e1f322d065512fe0
kunansy <kolobov.kirill@list.ru>
2021-08-20 16:58:02 +0300

    WordsToLearnListing schema fixed

--- vocabulary/words/schemas.py
+++ vocabulary/words/schemas.py
@@ -14,16 +14,5 @@ class WordToLearnResponse(WordToLearn):
     added_at: datetime.datetime
 
 
-class WordsToLearnListing(WordToLearn):
-    eng_t: list[constr(strip_whitespace=True)]
-    rus_t: list[constr(strip_whitespace=True)]
-
-    @validator('eng_t')
-    def validate_eng_t(cls,
-                       eng_t: str) -> list[str]:
-        return eng_t.split(';')
-
-    @validator('rus_t')
-    def validate_rus_t(cls,
-                       rus_t: str) -> list[str]:
-        return rus_t.split(';')
+class WordsToLearnListing(WordToLearnResponse):
+    words: list[WordToLearnResponse]
