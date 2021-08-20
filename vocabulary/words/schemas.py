4ec9022c0af9ec5ab3217d28a1839d953d3558e4
kunansy <kolobov.kirill@list.ru>
2021-08-20 16:44:20 +0300

    schemas fixed

--- vocabulary/words/schemas.py
+++ vocabulary/words/schemas.py
@@ -6,20 +6,20 @@ from pydantic import BaseModel, constr, validator
 
 class WordToLearn(BaseModel):
     word_id: UUID
-    word: constr(strip_whitespase=True)
+    word: constr(strip_whitespace=True)
     added_at: datetime.datetime
 
 
 class WordsToLearnListing(WordToLearn):
-    eng_t: list[constr(strip_whitespase=True)]
-    rus_t: list[constr(strip_whitespase=True)]
+    eng_t: list[constr(strip_whitespace=True)]
+    rus_t: list[constr(strip_whitespace=True)]
 
     @validator('eng_t')
-    def validate(cls,
-                 eng_t: str) -> list[str]:
+    def validate_eng_t(cls,
+                       eng_t: str) -> list[str]:
         return eng_t.split(';')
 
     @validator('rus_t')
-    def validate(cls,
-                 rus_t: str) -> list[str]:
+    def validate_rus_t(cls,
+                       rus_t: str) -> list[str]:
         return rus_t.split(';')
