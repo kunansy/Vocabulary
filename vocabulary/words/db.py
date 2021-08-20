323216cba8a617d85c16f2910d4a443f3efd9227
kunansy <kolobov.kirill@list.ru>
2021-08-20 16:51:24 +0300

    db.delete_word_to_learn() implemented

--- vocabulary/words/db.py
+++ vocabulary/words/db.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from uuid import UUID
 
 from sqlalchemy.engine import RowMapping
 import sqlalchemy.sql as sa
@@ -15,3 +16,13 @@ async def get_words_to_learn(*,
 
     async with database.session() as ses:
         return (await ses.execute(stmt)).mappings().all()
+
+
+async def delete_word_to_learn(*,
+                               word_id: UUID) -> Optional[RowMapping]:
+    stmt = sa.delete(models.WordToLearn)\
+        .returning(models.WordToLearn)\
+        .where(models.WordToLearn.word_id == word_id)
+
+    async with database.session() as ses:
+        return (await ses.execute(stmt)).mappings().one_or_none()
