bd1d6054a1ec78e7730ccdb72ce1a8714f314a92
kunansy <kolobov.kirill@list.ru>
2021-08-21 17:46:50 +0300

    /corpus/{word} implemented

--- vocabulary/examples/routes.py
+++ vocabulary/examples/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Query
 from vocabulary.examples import schemas
 from fastapi import APIRouter, Query
 
-from vocabulary.examples import schemas
+from vocabulary.examples import schemas, db
 
 
 router = APIRouter(
@@ -15,8 +15,11 @@ router = APIRouter(
 @router.get('/coprus/{word}',
             response_model=schemas.CorpusExamples)
 async def get_corpus_examples(word: str,
+                              pages_count: int = Query(10, ge=1),
                               language: schemas.LANGUAGES = Query('en')):
-    pass
+    return await db.get_corpus_examples(
+        word=word, mycorp=language, pages_count=pages_count
+    )
 
 
 @router.get('/self/{word}',
