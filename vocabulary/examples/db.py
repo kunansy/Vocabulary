1a4c8fa3bd1cfb941c2373b24923155578daa14a
kunansy <kolobov.kirill@list.ru>
2021-08-22 18:48:24 +0300

    db.get_corpus_examples() implemented

--- vocabulary/examples/db.py
+++ vocabulary/examples/db.py
@@ -1,14 +1,25 @@
+from typing import Any
+
 import rnc
-from sqlalchemy.engine import RowMapping
 
 
 async def get_corpus_examples(*,
                               mycorp: str,
                               word: str,
-                              pages_count: int) -> list[RowMapping]:
+                              pages_count: int) -> list[dict[str, Any]]:
     corp = rnc.ParallelCorpus(
         word, pages_count,
         mycorp=rnc.mycorp[mycorp], marker=str.upper
     )
     await corp.request_examples_async()
-    return list(corp)
+    return [
+        {
+            'original': ex.en,
+            'native': ex.ru,
+            'src': ex.src,
+            'ambiguation': ex.ambiguation,
+            'doc_url': ex.doc_url,
+            'found_wordforms': ex.found_wordforms
+         }
+        for ex in corp
+    ]
