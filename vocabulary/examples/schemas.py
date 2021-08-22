12bf7804394782f7b8c833c176c7ea8ef6780818
kunansy <kolobov.kirill@list.ru>
2021-08-22 18:45:57 +0300

    CorpusExample schema created

--- vocabulary/examples/schemas.py
+++ vocabulary/examples/schemas.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, validator
 from rnc import mycorp
 
 
@@ -8,6 +8,21 @@ LANGUAGES = Literal[tuple(
     k for k in mycorp.Parallel.__dict__.keys()
     if k[0].islower()
 )]
+
+
+class CorpusExample(BaseModel):
+    original: str
+    native: str
+    src: str
+    ambiguation: bool
+    doc_url: HttpUrl
+    found_wordforms: set[str]
+
+    @validator('ambiguation', pre=True)
+    def validate_ambiguation(cls,
+                             amb: str) -> bool:
+        print(amb)
+        return amb.lower() == 'disambiguated'
 
 
 class CorpusExamples(BaseModel):
