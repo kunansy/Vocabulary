269696afe274b89e26a95cb0122e6c67c855c4d4
kunansy <kolobov.kirill@list.ru>
2021-09-12 20:47:58 +0300

    some default values removed

--- vocabulary/common/settings.py
+++ vocabulary/common/settings.py
@@ -4,15 +4,14 @@ from environs import Env
 env = Env()
 env.read_env()
 
-# URL and model of synonyms searching
 SYNONYMS_SEARCH_URL = 'https://rusvectores.org/tayga_upos_skipgram_300_2_2019/' \
                       '{word}/api/json/'
 CORPUS_EXAMPLES_MARKER = lambda ex: f"<b>{ex.upper()}</b>"
 
 with env.prefixed('API_'):
     API_VERSION = env('VERSION', '0.1.0')
-    API_HOST = env('HOST', '127.0.0.1')
-    API_PORT = env.int('PORT', 8000)
+    API_HOST = env('HOST')
+    API_PORT = env.int('PORT')
     API_DEBUG = env.bool('DEBUG', False)
 
 with env.prefixed('LOGGER_'):
@@ -23,9 +22,9 @@ with env.prefixed('DB_'):
     DB_DSN_TEMPLATE = "postgresql+{driver}://{username}:{password}@" \
                       "{host}:{port}/{name}"
 
-    DB_HOST = env('HOST', '127.0.0.1')
-    DB_PORT = env.int('PORT', 5432)
-    DB_USERNAME = env('USERNAME', 'docker')
-    DB_PASSWORD = env('PASSWORD', 'docker')
-    DB_NAME = env('NAME', 'vocabulary')
+    DB_HOST = env('HOST')
+    DB_PORT = env.int('PORT')
+    DB_USERNAME = env('USERNAME')
+    DB_PASSWORD = env('PASSWORD')
+    DB_NAME = env('NAME')
     DB_ISOLATION_LEVEL = env('ISOLATION_LEVEL', 'REPEATABLE READ')
