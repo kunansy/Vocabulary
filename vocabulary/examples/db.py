7b954adcb9d3b5d4671d7377832f887ce4169a60
kunansy <kolobov.kirill@list.ru>
2021-09-26 10:59:58 +0300

    logs for requesting corpus examples added

--- vocabulary/examples/db.py
+++ vocabulary/examples/db.py
@@ -3,6 +3,7 @@ from typing import Any
 import rnc
 
 from vocabulary.common import settings
+from vocabulary.common.log import logger
 
 
 async def get_corpus_examples(*,
@@ -14,7 +15,9 @@ async def get_corpus_examples(*,
         mycorp=rnc.mycorp[mycorp],
         marker=settings.CORPUS_EXAMPLES_MARKER
     )
+    logger.info("Requesting corpus examples")
     await corp.request_examples_async()
+    logger.info("%s corpus examples got", len(corp))
 
     return [
         {
